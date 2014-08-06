e61479fbdbb4cfb9c3b639138bd07473465c5e40
= <thomas07@vt.edu>
2014-08-06 15:11:22 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Turtle::Application.config.secret_key_base = '4d463cf7289d42ca092c75474c5ab80115a2ca84fe13bd8307bd1127e32b4efe528808b013378492d30e3f611be48a50d8e82dd3a9f412a0a0457dd85ca88fd5'
